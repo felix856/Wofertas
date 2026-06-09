ccdbc08af262e2a358729d515e895eab18e2b032
Félix Francisco <82882273+felix856@users.noreply.github.com>
2026-06-08 23:22:42 -0300

    Update PageController.java

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -1,7 +1,8 @@
 package com.example.demo.controller;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Controller que serve as páginas HTML estáticas.
