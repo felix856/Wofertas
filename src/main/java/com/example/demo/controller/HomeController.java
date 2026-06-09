b8068a2dc95a162196f0c50694946c0f3b891bfe
Félix Francisco <82882273+felix856@users.noreply.github.com>
2026-06-08 23:20:30 -0300

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -18,49 +18,4 @@ public class HomeController {
     public String home() {
         return "forward:/index.html";
     }
-
-    @GetMapping("/login.html")
-    public String login() {
-        return "forward:/login.html";
-    }
-
-    @GetMapping("/mercadoHome.html")
-    public String mercadoHome() {
-        return "forward:/mercadoHome.html";
-    }
-
-    @GetMapping("/criar-oferta.html")
-    public String criarOferta() {
-        return "forward:/criar-oferta.html";
-    }
-
-    @GetMapping("/dashboard-analytics.html")
-    public String dashboardAnalytics() {
-        return "forward:/dashboard-analytics.html";
-    }
-
-    @GetMapping("/dashboard-pro.html")
-    public String dashboardPro() {
-        return "forward:/dashboard-pro.html";
-    }
-
-    @GetMapping("/historico.html")
-    public String historico() {
-        return "forward:/historico.html";
-    }
-
-    @GetMapping("/mercado-cadastro.html")
-    public String mercadoCadastro() {
-        return "forward:/mercado-cadastro.html";
-    }
-
-    @GetMapping("/perfil_mercado.html")
-    public String perfilMercado() {
-        return "forward:/perfil_mercado.html";
-    }
-
-    @GetMapping("/reset-senha.html")
-    public String resetSenha() {
-        return "forward:/reset-senha.html";
-    }
 }
