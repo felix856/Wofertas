fde31295bc194907987129eccda22c6c2d3bac67
felix856 <felixfrancisco2007@gmail.com>
2026-06-16 20:57:25 -0300

    fix: atualizando urls para o novo dominio no railway

--- view/logout.js
+++
@@ -1,9 +0,0 @@
-function logout() {
-    // Remove token e outros dados
-    localStorage.removeItem("token");
-    localStorage.removeItem("usuario");
-    localStorage.clear(); // opcional, limpa tudo
-
-    // Redireciona para login
-    window.location.href = "login.html";
-}
